fcb9cc8428df7fd42cd95bbcde8b22f40e7881aa
thatslifex <kevin.novotny@hotmail.com>
2025-02-21 19:05:23 +0100

    fixed typo

--- WoWLauncher/Updater/UpdateController.cs
+++ WoWLauncher/Updater/UpdateController.cs
@@ -85,7 +85,7 @@ namespace WoWLauncher.Updater
         public void RetrieveRealmIP()
         {
             // Set default and prepare folders
-            m_RealmAddress = " 46.4.132.200";
+            m_RealmAddress = "46.4.132.200";
             if (!Directory.Exists("Data/enUS"))
                 Directory.CreateDirectory("Data/enUS");
 
